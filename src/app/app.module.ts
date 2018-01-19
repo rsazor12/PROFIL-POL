78660c98d801852ed6c294a6b0adb046e6e26e72
Kamil Bielski <bielskikamil@gmail.com>
2018-01-20 00:49:14 +0100

    git informer done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap';
+import { SliderComponent } from './start/slider/slider.component';
+import { JumbotronComponent } from './start/jumbotron/jumbotron.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { StartPageInformerComponent } from './start/start-page-informer/start-page-informer.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    JumbotronComponent,
+    NavbarComponent,
+    StartPageInformerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
